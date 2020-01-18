0192cde4d55216fee1123635f7972036db1347c4
Zainal Abidin <55288692+zcoolacc@users.noreply.github.com>
2020-01-19 00:43:41 +0700

    Add files via upload

---
+++ Cetakstring.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cetakstring;
+
+/**
+ *
+ * @author zcool
+ */
+public class Cetakstring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Assalamualaikum guys");
+        System.out.println("salam kenal ");
+        System.out.print("Nama Saya Zainal");// TODO code application logic here
+    }
+    
+}
